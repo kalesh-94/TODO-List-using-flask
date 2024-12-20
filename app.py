383176326f5836cf268af81d8b8403761f4efd8e
Kalesh Patil <159576620+kalesh-94@users.noreply.github.com>
2024-12-20 21:55:02 +0530

    port changing

--- app.py
+++ app.py
@@ -67,5 +67,6 @@ def delete(sno):
     return redirect("/")
 # Run the application
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)  # Replace 5000 with your desired port
+
     
